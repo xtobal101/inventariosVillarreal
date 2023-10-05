41268b146e19593d1b1e759f1150fdc79ab6f6f4
xtobal101 <96153850+xtobal101@users.noreply.github.com>
2023-10-05 12:06:13 -0600

    Second commit

---
+++ BackEnd/myproject/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = {'name':'Dennis', 'age':28}
+    return Response(person)
